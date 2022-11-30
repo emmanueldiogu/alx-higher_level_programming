57c941133522bfbb9a285a1fd25851eb794fff1e
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-11-30 07:22:02 +0100

    is lower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if type(c) == str and len(c) == 1:
-        if (ord(c) >= 97 and ord(c) < 123):
-            return True
-        else:
-            return False
+    if (ord(c) >= 97 and ord(c) < 123):
+        return True
     else:
         return False

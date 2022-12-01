61988a2bcb753abc60edc84e0abb3a2451e1c556
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-12-01 22:17:43 +0100

    args pycodestyle resolved

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,8 @@ from sys import argv
 
 
 def do_something():
-    print("{} {}".format(len(argv[1:]), 'argument:' if (len(argv[1:]) == 1) else 'arguments:'))
+    print("{} {}".format(len(argv[1:]), 'argument:' if (
+        len(argv[1:]) == 1) else 'arguments:'))
     count = 1
     for i in argv[1:]:
         print("{}: {}".format(count, i))

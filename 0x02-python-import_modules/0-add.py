0e0b8e33d6699fd86e59aaa414fe83fbcd1f33e5
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-12-01 18:23:05 +0100

    improve none import statement

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
 
-print("{} + {} = {}".format(a, b, add(a=a, b=b)))
+
+def do_something():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a=a, b=b)))
+
+
+if __name__ == "__main__":
+    do_something()

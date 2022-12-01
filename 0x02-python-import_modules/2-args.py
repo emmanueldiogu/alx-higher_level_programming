6526a7a1cb0009b88697cc1dcf08918efcdb3338
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-12-01 21:41:02 +0100

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@
 from sys import argv
 
 
-print("{} arguments".format(len(argv[1:])))
+print("{} {}".format(len(argv[1:]), 'argument' if (len(argv[1:]) == 1) else 'arguments'))
 count = 1
 for i in argv[1:]:
     print("{}: {}".format(count, i))

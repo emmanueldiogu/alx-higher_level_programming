62e60be6c3c661fd557766e24158a5136dd071a7
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-12-01 21:45:26 +0100

    args update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,12 @@
 from sys import argv
 
 
-print("{} {}".format(len(argv[1:]), 'argument:' if (len(argv[1:]) == 1) else 'arguments:'))
-count = 1
-for i in argv[1:]:
-    print("{}: {}".format(count, i))
-    count += 1
+def do_something():
+    print("{} {}".format(len(argv[1:]), 'argument:' if (len(argv[1:]) == 1) else 'arguments:'))
+    count = 1
+    for i in argv[1:]:
+        print("{}: {}".format(count, i))
+        count += 1
+
+if __name__ == "__main__":
+    do_something()

d706ec45cceb36da52932d40bf170127adaa5874
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-12-02 04:40:56 +0100

    my cal

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 
-from sys import argv
+from sys import argv, exit
 from calculator_1 import add, sub, mul, div
 
 def basic_operations():
-    operators = ['*', '-', '+', '/']
-    print(len(argv[1:]))
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+
     if len(argv[1:]) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif len(argv[2]) not in operators:
+    elif not argv[2] in ops.keys():
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    pass
+
+    a = int(argv[1])
+    b = int(argv[3])
+    
+    print("{} {} {} = {}".format(a, argv[2], b, ops[argv[2]](a, b)))
 
 
 if __name__ == "__main__":

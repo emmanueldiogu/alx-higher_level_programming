18e443d3cc2f264f64c135d439a20af4812bbfe8
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-12-07 03:11:09 +0100

    print list integer update

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,6 +4,3 @@ def print_list_integer(my_list=[]):
     for item in range(len(my_list)):
         print("{:d}".format(my_list[item]))
     return ''
-
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)

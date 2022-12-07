d63dce5105697a634c8cc1e727644a373415f136
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-12-07 04:05:54 +0100

    print matrix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,3 @@
 def multiple_returns(sentence):
     tuple_1 = (len(sentence), sentence[0])
     return tuple_1
-
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))

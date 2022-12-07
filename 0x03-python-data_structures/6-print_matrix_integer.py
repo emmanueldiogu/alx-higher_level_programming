44ce2adba7f73957da3cca6c14cdc628a3fc0069
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-12-07 03:56:11 +0100

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for item in matrix:
-        for sub in item:
-            # if 
-            print("{}".format(sub), end='')
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
         print('')
 
 matrix = [

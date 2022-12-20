4f16ebf9842c44b1a191992fb7a29e7a48d60604
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-12-20 18:23:37 +0100

    four square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Define square"""
+
+
+class Square:
+    """Defines the area of a square"""
+
+    def __init__(self, size=0):
+        if (type(size) is not int):
+            raise (TypeError('size must be an integer'))
+        elif (size < 0):
+            raise (ValueError('size must be >= 0'))
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """Private property getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Private property setter"""
+        if (type(value) is not int):
+            raise (TypeError('size must be an integer'))
+        elif (value < 0):
+            raise (ValueError('size must be >= 0'))
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculate square of size"""
+        return self.__size ** 2

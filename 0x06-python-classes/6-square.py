7d168ec9203cadbf6856738cf79af5e8abbc47e3
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-12-20 19:27:24 +0100

    six square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,12 +34,13 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
+        """Print square matrix"""
         if self.__size > 0:
             for blank in range(self.position[1]):
                 print()
             for i in range(self.__size):
-                print(" "*self.position[0], end='')
-                print('#' * self.__size)
+                print(" " * self.position[0], end='')
+                print("#" * self.__size)
         else:
             print()
 
@@ -51,7 +52,12 @@ class Square:
     @position.setter
     def position(self, value):
         """Private position setter"""
-        if (len(value) != 2) or (type(value) is not tuple) or (type(value[0]) is not int) or (type(value[1]) is not int) or (value[0] < 0) or (value[1] < 0):
+        if (len(value) != 2) \
+            or (type(value) is not tuple) \
+                or (type(value[0]) is not int) \
+        or (type(value[1]) is not int) \
+        or (value[0] < 0) \
+        or (value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value

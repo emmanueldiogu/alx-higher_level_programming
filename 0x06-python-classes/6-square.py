45d5b51ab245b845a599bfc4612994702f8198fd
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-12-21 05:23:22 +0100

    six square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -52,12 +52,10 @@ class Square:
     @position.setter
     def position(self, value):
         """Private position setter"""
-        if (len(value) != 2) \
-            or (type(value) is not tuple) \
+        if (len(value) != 2) or (type(value) is not tuple) \
                 or (type(value[0]) is not int) \
-        or (type(value[1]) is not int) \
-        or (value[0] < 0) \
-        or (value[1] < 0):
+                or (type(value[1]) is not int) \
+                or (value[0] < 0) or (value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value

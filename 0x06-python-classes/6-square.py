0284b9c15ca14dde6145d84a051b5882cd1e9319
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-12-21 05:50:00 +0100

    six square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,41 +11,38 @@ class Square:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """Private property getter"""
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """Private property setter"""
-        if (type(value) is not int):
-            raise (TypeError('size must be an integer'))
-        elif (value < 0):
-            raise (ValueError('size must be >= 0'))
-        else:
-            self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     @property
     def position(self):
-        """Private position getter"""
-        return self.__position
+        """Get/set the current position of the square."""
+        return (self.__position)
 
     @position.setter
     def position(self, value):
-        """Private position setter"""
-        if (not isinstance(value, tuple) or len(value) != 2
-                or all(isinstance(num, int) for num in value)
-                or all(num >= 0 for num in value)):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        self.__position = value
 
     def area(self):
-        """Calculate square of size"""
+        """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):

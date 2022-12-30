c10b968704f5f042b47a2a11f93c65a7efd8f45b
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-12-30 07:16:10 +0100

    2 matrix divided

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,13 +5,6 @@ rounded to 2 decimal places and
 return a new matrix
 """
 
-arr1 = [
-    [1, 2, 3],
-    [4, 5, 1],
-    [4, "5", 1],
-    [4, 5, 1],
-]
-
 
 def matrix_divided(matrix, div):
     """Function that divides all elements of a matrix
@@ -24,17 +17,19 @@ def matrix_divided(matrix, div):
     for i in range(len(matrix)):
         if (sizeofmat != len(matrix[i])):
             raise TypeError("Each row of the matrix must have the same size")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
-
+    new_mat = []
     for row in matrix:
+        new_col = []
         for col in row:
             if type(col) not in [int, float]:
                 raise TypeError(
                     "matrix must be a matrix (list of lists) of integers/floats")
-
-
-matrix_divided(arr1, 3)
+            new_col.append(round(col / div, 2))
+        new_mat.append(new_col)
+    return new_mat

fe1cb0501f3c58af86dbe568b963798df4aca1c3
Emmanuel Diogu <emmanueldiogu@gmail.com>
2023-05-16 04:15:09 -0400

    if not exist db

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,4 @@
-CREATE DATABASE "hbtn_0c_0";
+-- Task: Create a database called 'hbtn_0c_0'
+
+-- Create the 'hbtn_0c_0' database if it doesn't already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;

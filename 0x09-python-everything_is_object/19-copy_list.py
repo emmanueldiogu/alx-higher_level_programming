09b37b897482b818967d8a05859e767dcfbafd70
Emmanuel Diogu <emmanueldiogu@gmail.com>
2024-01-30 11:37:17 -0500

    update copy list

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
-"""copy a list object
-"""
-
-
 def copy_list(l):
-    """Function that returns a copy of a list
-
-    Args:
-        l (list): any list
-
-    Returns:
-        list: copy of list
-    """
     return l[:]
